2cc1b6103690ae68f318c6ba928504ebeb1bfd02
dude-s14 <dude_s14@live.ca>
2020-12-09 15:51:22 -0500

    Setup user validation in the Get mappings for the admin controller

--- LoginDemo/demo/src/main/java/comp31/demo/controller/AdminController.java
+++ LoginDemo/demo/src/main/java/comp31/demo/controller/AdminController.java
@@ -4,43 +4,69 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import comp31.demo.model.User;
 import comp31.demo.repo.ShowRepo;
 import comp31.demo.repo.UserRepo;
+import comp31.demo.service.LoginService;
 
 @Controller
+@SessionAttributes("currentUser")
 public class AdminController {
+
+    @Autowired LoginService loginService;
     @Autowired ShowRepo showRepo;
     @Autowired UserRepo userRepo;
 
-    private Boolean validUser(User user){
+    private Boolean isValid(User user){
         Boolean result = false;
-        
-
+        if (user != null && loginService.validate(user.getUserName()) != null && user.getUserRole().equals("admin"))
+            result = true;
         return result;
     }
 
     @GetMapping("/admin")
     public String admin(Model model){
-        String nextPage = "adminpage";
-
-
+        String nextPage = "login";
+        User currentUser = (User) model.getAttribute("currentUser");
+        if (isValid(currentUser))
+            nextPage = "adminpage";
+        else{
+            User user = new User();
+            model.addAttribute("currentUser", null);
+            model.addAttribute("newUser", user);
+        }
         return nextPage;
     }
 
     @GetMapping("/adminUserList")
     public String adminUList(Model model){
-        String nextPage = "adminUserList";
-
-        
+        String nextPage = "login";
+        User currentUser = (User) model.getAttribute("currentUser");
+        if (isValid(currentUser)){
+            nextPage = "adminUserList";
+            // model.addAttribute("userList", userRepo.findAll());
+        } else{
+            User user = new User();
+            model.addAttribute("currentUser", null);
+            model.addAttribute("newUser", user);
+        }
         return nextPage;
     }
 
     @GetMapping("/adminShowList")
     public String adminSList(Model model){
-        String nextPage = "adminShowList";
-        
+        String nextPage = "login";
+        User currentUser = (User) model.getAttribute("currentUser");
+        if (isValid(currentUser)){
+            nextPage = "adminShowList";
+            // model.addAttribute("showList", showRepo.findAll());
+        } else{
+            User user = new User();
+            model.addAttribute("currentUser", null);
+            model.addAttribute("newUser", user);
+        }
         return nextPage;
     }
 }
