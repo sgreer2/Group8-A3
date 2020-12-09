cd9b211682a4de77faae03a89d1dc2704b0c93a8
dude-s14 <dude_s14@live.ca>
2020-12-09 14:39:58 -0500

    Created all all pages needed for admin role, and setup the basic GetMapping for them

--- LoginDemo/demo/src/main/java/comp31/demo/controller/AdminController.java
+++ LoginDemo/demo/src/main/java/comp31/demo/controller/AdminController.java
@@ -1,8 +1,46 @@
 package comp31.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import comp31.demo.model.User;
+import comp31.demo.repo.ShowRepo;
+import comp31.demo.repo.UserRepo;
 
 @Controller
 public class AdminController {
-    
+    @Autowired ShowRepo showRepo;
+    @Autowired UserRepo userRepo;
+
+    private Boolean validUser(User user){
+        Boolean result = false;
+        
+
+        return result;
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model){
+        String nextPage = "adminpage";
+
+
+        return nextPage;
+    }
+
+    @GetMapping("/adminUserList")
+    public String adminUList(Model model){
+        String nextPage = "adminUserList";
+
+        
+        return nextPage;
+    }
+
+    @GetMapping("/adminShowList")
+    public String adminSList(Model model){
+        String nextPage = "adminShowList";
+        
+        return nextPage;
+    }
 }
